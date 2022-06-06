49b97eb4dc7bf0e9ba8f3ce8a841d75943b4b95d
Raag2005 Oskar J. W. Raagart <oskar@raagart.dk>
2022-06-06 12:18:39 -0700

    Create client.js

---
+++ client.js
@@ -0,0 +1,34 @@
+/* 
+    Use this as a client script
+    <script async src="client.js"></script>
+
+    Example
+    let { get } = new fxCore()
+    let bankAmount = await get("getBankAmount")
+
+    / 
+
+    let { request, get } = new fxCore()
+    request("transferMoney", {
+        number: get("getPhoneNumber"),
+        numberTo: "12345678"
+    })
+*/
+
+class fxCore {
+    get(get) {
+        let result = await fetch("https://fx-services/get"+get)
+        return result.json()
+    }
+
+    request(request, data) {
+        let result = await fetch("https://fx-services/request"+request, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        return result.json()
+    }
+}
